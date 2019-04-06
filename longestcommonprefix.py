c32e22c0e011f1fc56e29a1370d5966aa2529a90
lilacnblue <40076568+lilacnblue@users.noreply.github.com>
2019-04-06 16:43:52 +0530

    Create longestcommonprefix.py

---
+++ longestcommonprefix.py
@@ -0,0 +1,14 @@
+n=int(input("")) 
+a = []
+for i in range (n):
+    x=input()
+    a.insert(i,x)
+    i+=1
+lp=""
+sw = min(a, key=len)
+for i in range(len(sw)):
+	if all ([x.startswith(sw[:i+1]) for x in a]):
+		lp=sw[:i+1]
+	else:
+		break
+print(p)
