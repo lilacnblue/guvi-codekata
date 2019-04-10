2f0900124916fb4841087791666a7fe87d917092
lilacnblue <40076568+lilacnblue@users.noreply.github.com>
2019-04-10 09:23:21 +0530

    Update catalan.py

--- catalan.py
+++ catalan.py
@@ -1,7 +1,3 @@
-n=int(input())
-
-for i in range(n):
-	print(c(i),end=" ")
 
 def c(n):
 	if n <= 1:
@@ -12,6 +8,11 @@ def c(n):
 		r+=c(i)*c(n-i-1)
 	
 	return r 	
-	
+
+n=int(input())
+
+for i in range(n):
+	print(c(i),end=" ")
+
 	
 	
