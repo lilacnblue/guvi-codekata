33c5e36988763713e523bc3be097a260543175d3
lilacnblue <40076568+lilacnblue@users.noreply.github.com>
2019-07-07 20:55:58 +0530

    Update bon.py

--- bon.py
+++ bon.py
@@ -2,9 +2,9 @@ n,k=input().split()
 k=int(k)
 arr=[int(x) for x in input().split()]
 val=int(input())
-o=val-(sum(arr)-arr[k])/2
-if(o==0):
+p=val-(sum(arr)-arr[k])/2
+if(p==0):
 	print("Bon Appetit")
 else:
 	assert val==sum(arr)/2
-	print(int(o))
+	print(int(p))
