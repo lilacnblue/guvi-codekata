f8d8b2e20512b8abb98b3517d81fdeeaea93f5af
lilacnblue <40076568+lilacnblue@users.noreply.github.com>
2019-07-07 13:52:58 +0530

    Update house67.py

--- house67.py
+++ house67.py
@@ -1,7 +1,7 @@
-i=[int(x) for x in input().split()]
-if i[0]-i[1]==0:
+k=[int(x) for x in input().split()]
+if k[0]-k[1]==0:
   print("0",end=" ")
   print("0")
 else:
   print("1",end=" ")
-  print(i[0]-i[1])
+  print(k[0]-k[1])
