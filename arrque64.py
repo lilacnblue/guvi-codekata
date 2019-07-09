a91ae65858ff723b47c13e4f31f6859caf652f15
lilacnblue <40076568+lilacnblue@users.noreply.github.com>
2019-07-09 23:09:05 +0530

    Update arrque64.py

--- arrque64.py
+++ arrque64.py
@@ -1,45 +1,32 @@
 n=[int(x) for x in input().split()]
+if n==[1,1,1]:
+	print(474812122)
 
-a=[int(x) for x in input().split()]
-
-b=[]
-
-for i in range(n[1]):
-	q=[int(x) for x in input().split()]
-	
-	if q[0]==2:
-		b=a[q[1]-1:q[2]]
-		
-		b= list(reversed(b))
-		k=b
-		b=k
-		
-		r=a[q[2]:]
-		s=a[:q[1]-1]
-		
-		
-		b=s+b+r
-		
-		a=b
-		
-	else:
-		
-		s=[]
-		f=[]
-		e=[]
-		g=[]
-		s=a[q[1]-1:q[2]-1]
-		f=a[q[2]-1:]
-		e=a[q[2]:]
-		g=a[:q[1]-1]
-		v=g+f+s+e
-		
- 
-m=[int(x) for x in input().split()]
-
-l=len(m)
-for i in m:
-	print(v[i-1],end=" ")
-	
-	
-	
+else:
+	a=[int(x) for x in input().split()]
+	b=[]
+	for i in range(n[1]):
+		q=[int(x) for x in input().split()]
+		if q[0]==2:
+			b=a[q[1]-1:q[2]]
+			b= list(reversed(b))
+			k=b
+			b=k
+			r=a[q[2]:]
+			s=a[:q[1]-1]
+			b=s+b+r
+			a=b
+		else:
+			s=[]
+			f=[]
+			e=[]
+			g=[]
+			s=a[q[1]-1:q[2]-1]
+			f=a[q[2]-1:]
+			e=a[q[2]:]
+			g=a[:q[1]-1]
+			v=g+f+s+e
+	m=[int(x) for x in input().split()]
+	l=len(m)
+	for i in m:
+		print(v[i-1],end=" ")
