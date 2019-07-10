53b4eca86c67d1c9eeab20263f89b53a37625ae7
lilacnblue <40076568+lilacnblue@users.noreply.github.com>
2019-07-10 10:01:59 +0530

    Update STRCON3.py

--- STRCON3.py
+++ STRCON3.py
@@ -3,11 +3,12 @@ a=x[0]
 n=len(a)
 b=x[1]
 m=len(b)
+t=min(n,m)
 c=0
-for i in range(n):
+for i in range(t):
 	if x[0][i]==x[1][i]:
 		c=c
 	elif x[0][i]!=x[1][i]:
 		c=c+1
-r=m-n+c
+r=abs(m-n)+c
 print(r)
