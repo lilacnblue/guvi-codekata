271dcf356999dbdd560b741bb0eb63d031890606
lilacnblue <40076568+lilacnblue@users.noreply.github.com>
2019-08-03 18:31:18 +0530

    Update stair10.py

--- stair10.py
+++ stair10.py
@@ -2,9 +2,26 @@ n=int(input())
 x=[int(i) for i in input().split()]
 s=0
 c=0
+a=0
+b=[0]
 for i in range(1,n):
 	if x[i-1]<x[i]:
-		(c)=(c)+x[i-1]
+		if a<x[i]:
+			(c)=(c)+x[i-1]+a
+			(s)=(s)+(c)
+			
+			
+		else :
+			(c)=(c)+x[i-1]
+			(s)=(s)+(c)
+	else :
+		for k in range(len(b)):
+			if x[i]<b[k]:
+				c=c-b[k]
+			else:
+				c=(c)
 		(s)=(s)+(c)
+		a=x[i-1]
+		b.append(a)
 print(s)
 		
